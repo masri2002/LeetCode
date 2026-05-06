fff6f3ab85f4b02c0325a3a37fc87d2e2b5ea14f
ahmad masri <ahmadmmasrri1@gmail.com>
2026-05-06 16:06:56 +0300

    chore: solve rotate list

--- src/main/java/org/example/rotateRight.java
+++ src/main/java/org/example/rotateRight.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class rotateRight {
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -19,7 +19,46 @@ public class rotateRight {
     }
 
     public ListNode rotateRight(ListNode head, int k) {
-        ListNode temp =head;
-        return temp;
+        if (head == null) return head;
+        if (head.next == null) return head;
+        ListNode check = head;
+        int n = 0;
+        while (check != null) {
+            n++;
+            check = check.next;
+        }
+        for (int i = 0; i < k % n; i++) {
+
+            ListNode temp = head;
+            while (temp != null) {
+                if (temp.next.next == null) {
+                    ListNode next = temp.next;
+                    temp.next = null;
+                    next.next = head;
+                    head = next;
+                }
+                temp = temp.next;
+            }
+        }
+        return head;
     }
+
+    public static void main(String[] args) {
+        ListNode one = new ListNode(1);
+        ListNode two = new ListNode(2);
+        one.next = two;
+        ListNode three = new ListNode(3);
+        two.next = three;
+        ListNode four = new ListNode(4);
+        three.next = four;
+        ListNode five = new ListNode(5);
+        four.next = five;
+        rotateRight l = new rotateRight();
+        ListNode vode = l.rotateRight(one, 2);
+        while (vode != null) {
+            System.out.println(vode.val);
+            vode = vode.next;
+        }
+    }
+
 }
